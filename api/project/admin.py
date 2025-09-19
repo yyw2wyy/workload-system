d6c7d7eda8a0f58267e00ce57b5311e8dcfe31e4
wyy <1877317151@qq.com>
2025-09-19 16:20:47 +0800

    project admin修改

--- api/project/admin.py
+++ api/project/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from django.utils.html import format_html
-from .models import Project
+from django.core.exceptions import ValidationError
+from django.forms.models import BaseInlineFormSet
+from .models import Project, ProjectShare
+
+class ProjectShareInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        # 过滤掉被删除的表单
+        active_forms = [form for form in self.forms if not form.cleaned_data.get('DELETE', False)]
+        if len(active_forms) < 1:
+            raise ValidationError("项目至少需要保留一位参与人。")
+
+
+class ProjectShareInline(admin.TabularInline):
+    model = ProjectShare
+    extra = 1  # 默认额外空行数
+    formset = ProjectShareInlineFormSet
+
 
 @admin.register(Project)
 class WorkloadAdmin(admin.ModelAdmin):
@@ -13,3 +29,5 @@ class WorkloadAdmin(admin.ModelAdmin):
     ]
     date_hierarchy = 'created_at'
     readonly_fields = ['created_at', 'updated_at']
+
+    inlines = [ProjectShareInline]
